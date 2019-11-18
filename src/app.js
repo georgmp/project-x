c61158f31da578b8ba8eac8f32a87bd389455bc8
Georg Preuss <preussg@gmail.com>
2019-11-18 16:37:27 +0000

    added pop-up info

--- src/app.js
+++ src/app.js
@@ -115,6 +115,33 @@ class App extends React.Component {
         })
       })
 
+      map.on('click', 'markers', function (e) {
+        const coordinates = e.features[0].geometry.coordinates.slice()
+        const description = e.features[0].properties.description
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360
+        }
+
+        new mapboxgl.Popup()
+          .setLngLat(coordinates)
+          .setHTML(description)
+          .addTo(map)
+      })
+
+      // Change the cursor to a pointer when the mouse is over the places layer.
+      map.on('mouseenter', 'markers', function () {
+        map.getCanvas().style.cursor = 'pointer'
+      })
+
+      // Change it back to a pointer when it leaves.
+      map.on('mouseleave', 'markers', function () {
+        map.getCanvas().style.cursor = ''
+      })
+
       console.log(formattedResponse)
     }
   }
